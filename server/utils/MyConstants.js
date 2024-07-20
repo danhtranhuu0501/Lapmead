784ad5a9ba91f80ac957e8f758f725c5150200d3
danhtranhuu0501 <94184693+danhtranhuu0501@users.noreply.github.com>
2024-07-21 00:21:19 +0700

    Lan1

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'clustersgp.zhilve1.mongodb.net',
+    DB_USER: 'sonkk',
+    DB_PASS: 'Abc123456',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'hoangthimaixuan',
+    JWT_EXPIRES: '36000000000', // in milliseconds
+    EMAIL_USER: 'danhtran131003@gmail.com', // gmail service
+    EMAIL_PASS: 'vigy odsi fdoh dvvo'
+    // EMAIL_PASS: 'abc'
+  };
+  module.exports = MyConstants;
